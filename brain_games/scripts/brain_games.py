8c154b6b8072fd0bfd3a3b91e80c6ab7c3e7a734
Ekaterina Pastina <kate.pastina@gmail.com>
2023-11-01 22:30:03 +0300

    add files at step three

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
+from brain_games.cli import welcome_user
+
+
 def greet():
-	print("Welcome to the Brain Games!");
+	print("Welcome to the Brain Games!")
 
 def main():
-	greet();
+	greet()
+	name = welcome_user()
+	print(f'Hello, {name}!')
 
 if __name__ == '__main__':
 	main()
