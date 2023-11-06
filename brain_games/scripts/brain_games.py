24efe0f287a11437c94d58b2131743ba35b7fefb
Ekaterina Pastina <kate.pastina@gmail.com>
2023-11-06 10:03:32 +0300

    add files at four step

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,12 +3,14 @@ from brain_games.cli import welcome_user
 
 
 def greet():
-	print("Welcome to the Brain Games!")
+    print("Welcome to the Brain Games!")
+
 
 def main():
-	greet()
-	name = welcome_user()
-	print(f'Hello, {name}!')
+    greet()
+    name = welcome_user()
+    print(f'Hello, {name}!')
+
 
 if __name__ == '__main__':
-	main()
+    main()
